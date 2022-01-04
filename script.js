e0c1e9c4ac2e2b595882ad249f689104b9427f5b
brypolanco <brpolanc@gmail.com>
2022-01-04 00:48:59 -0500

    Rearrange code so that event listeners for clicking and hovering are still active after clearing grid

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function makeGrid(row=16, column=16){
             gridRow.appendChild(gridItem).className='gridItem';
         }
     }
+    document.querySelectorAll('.gridItem').forEach(item=>item.addEventListener('mousedown', setDraw));
+    document.querySelectorAll('.gridItem').forEach(item=>item.addEventListener('mouseover', draw));
 }
 
 makeGrid();
@@ -19,11 +21,9 @@ makeGrid();
 function setDraw(e){
     e.target.style.backgroundColor = 'purple';
     positionClick = true;
-    console.log(`positionClick in setDraw: ${positionClick}`);
 }
 
 function draw(e){
-    console.log(`positionClick in draw function b4 if: ${positionClick}`);
     if(positionClick===true){
         e.target.style.backgroundColor = 'purple';
     }
@@ -31,26 +31,15 @@ function draw(e){
 }
 
 function clearGrid(){
-    gridItemList.forEach(item=>item.style.backgroundColor='');
     positionClick=false;
-    console.log(`positionClick in clearGrid: ${positionClick}`);
-    gridItemList.forEach(item=>item.remove());
-    gridRowList.forEach(item=>item.remove());
-    
+    document.querySelectorAll('.gridItem').forEach(item=>item.style.backgroundColor='');
+    document.querySelectorAll('.gridItem').forEach(item=>item.parentNode.removeChild(item));
+    document.querySelectorAll('.row').forEach(item=>item.parentNode.removeChild(item));
+
     row = window.prompt('Enter number of rows up to 100.');
     column =  window.prompt('Enter number of columns up to 100.');
     makeGrid(row, column);
 }
 
-
-
-const gridRowList = document.querySelectorAll('.row');
-console.log('Row List');
-console.log(gridRowList);
-const gridItemList = document.querySelectorAll('.gridItem');
-gridItemList.forEach(item=>item.addEventListener('mousedown', setDraw));
-gridItemList.forEach(item=>item.addEventListener('mouseover', draw));
-
 const btnClear = document.querySelector('#clear');
 btnClear.addEventListener('click', clearGrid);
-
